4d4b31456cbd6d779dfcddaa32553fe24ea0d2e9
Ryo Ota <nwtgck@gmail.com>
2017-11-16 09:48:20 +0900

    [fix] Fix the file name to download

--- math2img.rb
+++ math2img.rb
@@ -3,7 +3,7 @@ MATH2IMG_VERSION="0.3.0"
 
 class Math2img < Formula
     desc "Convert LaTeX Math to image"
-    url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/math2img-v#{MATH2IMG_VERSION}macOS"
+    url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/math2img-v#{MATH2IMG_VERSION}-macOS"
     sha256 "ecbea5dfbd226398f6db212b3a9c330e6d266577e467781a78028d9ab284cc91"
     # gsha256sum <binary>
 
