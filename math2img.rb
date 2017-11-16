80ff8b9952e9d72b614dcff99b3bbe737a1bf6f5
Ryo Ota <nwtgck@gmail.com>
2017-11-16 09:29:58 +0900

    [bump] Bump up version to 0.3.0

--- math2img.rb
+++ math2img.rb
@@ -1,10 +1,10 @@
 
-MATH2IMG_VERSION="0.2.1"
+MATH2IMG_VERSION="0.3.0"
 
 class Math2img < Formula
     desc "Convert LaTeX Math to image"
     url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/math2img-macOS"
-    sha256 "ee5a86324102e3ec5dac5bffd4d394b810c90cd2a677cea20c24a6b411d59978"
+    sha256 "ecbea5dfbd226398f6db212b3a9c330e6d266577e467781a78028d9ab284cc91"
     # gsha256sum <binary>
 
     def install
