8d9c70eabdf6a4faef1e5fae3fcf0c273044c737
Ryo Ota <nwtgck@gmail.com>
2017-11-16 09:50:14 +0900

    [fix] Fix src file name of `mv`

--- math2img.rb
+++ math2img.rb
@@ -1,14 +1,15 @@
 
-MATH2IMG_VERSION="0.3.0"
+MATH2IMG_VERSION = "0.3.0"
+APP_FILE_NAME    = "math2img-v#{MATH2IMG_VERSION}-macOS"
 
 class Math2img < Formula
     desc "Convert LaTeX Math to image"
-    url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/math2img-v#{MATH2IMG_VERSION}-macOS"
+    url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/#{APP_FILE_NAME}"
     sha256 "ecbea5dfbd226398f6db212b3a9c330e6d266577e467781a78028d9ab284cc91"
     # gsha256sum <binary>
 
     def install
-        system("mv math2img-macOS math2img")
+        system("mv #{APP_FILE_NAME} math2img")
         bin.install 'math2img'
     end
 
