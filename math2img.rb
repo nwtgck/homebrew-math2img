31d34bbf9b47239454bdfdaf80d870fa38136773
Ryo Ota <nwtgck@gmail.com>
2018-01-11 00:51:03 +0900

    [update] Update for v0.4.0

--- math2img.rb
+++ math2img.rb
@@ -1,12 +1,12 @@
 
-MATH2IMG_VERSION = "0.3.0"
+MATH2IMG_VERSION = "0.4.0"
 APP_FILE_NAME    = "math2img-v#{MATH2IMG_VERSION}-macOS"
 
 class Math2img < Formula
     desc "Convert LaTeX Math to image"
     url "https://github.com/nwtgck/math2img/releases/download/v#{MATH2IMG_VERSION}/#{APP_FILE_NAME}"
-    sha256 "ecbea5dfbd226398f6db212b3a9c330e6d266577e467781a78028d9ab284cc91"
-    # gsha256sum <binary>
+    sha256 "4789d779a105d9500d1e9167c878507ce6c8fef7e4814af5496354811caa3d23"
+    # shasum -a 256 <binary>
 
     def install
         system("mv #{APP_FILE_NAME} math2img")
